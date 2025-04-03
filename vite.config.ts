5bc78dcfc01ccb5d8978705ebb2e2936dab99863
tachyon6 <songjunjun62754@gmail.com>
2025-04-03 21:30:00 +0900

    feat: Sitemap 플러그인 추가 및 robots.txt에 사이트맵 위치 명시

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import Sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
     base: "/",
-    plugins: [react()],
+    plugins: [
+        react(),
+        Sitemap({ 
+            hostname: "https://boltlab.co",
+            dynamicRoutes: ["/", "/portfolio"]
+        })
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
